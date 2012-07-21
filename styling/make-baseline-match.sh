08bf948a7ee41040e41a1f806c7da58644b33532
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-07-22 01:43:34 +0900

    chapter, section, subsection の余白調節は完了した
    一方、itemize や figure や minipage などの余白調整が無理ゲー

--- styling/make-baseline-match.sh
+++ styling/make-baseline-match.sh
@@ -3,7 +3,7 @@
 files=""
 para=0
 
-for page in $(seq 1 20); do
+for page in $(seq 1 41); do
 	next_page=$((page+1))
 	pdftk page$page.pdf stamp page$next_page.pdf output match$page.pdf &
 	files="$files match${page}.pdf"
@@ -16,4 +16,4 @@ for page in $(seq 1 20); do
 done
 
 pdftk $files cat output baseline-match.pdf
-rm match*.pdf page*.pdf
+# rm match*.pdf page*.pdf
