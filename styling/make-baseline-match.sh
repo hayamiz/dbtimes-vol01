478f34988b74769acfbdeddfb1bacad404484b7b
Yuto Hayamizu <y.hayamizu@gmail.com>
2012-07-22 00:48:12 +0900

    余白調整中

---
+++ styling/make-baseline-match.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+files=""
+para=0
+
+for page in $(seq 1 20); do
+	next_page=$((page+1))
+	pdftk page$page.pdf stamp page$next_page.pdf output match$page.pdf &
+	files="$files match${page}.pdf"
+	if [ $para -lt 8 ]; then
+		para=$((para+1))
+	else
+		wait
+		para=0
+	fi
+done
+
+pdftk $files cat output baseline-match.pdf
+rm match*.pdf page*.pdf
